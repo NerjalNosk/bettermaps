a3830699137d106a204e4547b36ae609d841df32
Nerjal Nosk <nosk.nerjal@gmx.com>
2024-01-17 02:34:17 +0100

    C²ME 1.20 patch

--- src/main/java/com/nerjal/bettermaps/mixins/compat/c2me/present/CheckedThreadLocalRandomMixin.java
+++ src/main/java/com/nerjal/bettermaps/mixins/compat/c2me/present/CheckedThreadLocalRandomMixin.java
@@ -8,7 +8,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.function.Supplier;
@@ -21,20 +20,11 @@ public class CheckedThreadLocalRandomMixin extends LocalRandom {
         super(l);
     }
 
-    @Inject(method = "setSeed", at = @At("HEAD"), cancellable = true)
-    private void onSetSeedCheckIfMapLocationThread(long seed, CallbackInfo ci) {
+    @Inject(method = "isSafe", at = @At("HEAD"), cancellable = true)
+    private void onSetSeedCheckIfMapLocationThread(CallbackInfoReturnable<Boolean> cir) {
         Thread t = this.owner != null ? this.owner.get() : null;
         if (Bettermaps.locateMapTaskThreads.containsValue(t)) {
-            super.setSeed(seed);
-            ci.cancel();
-        }
-    }
-
-    @Inject(method = "next", at = @At("HEAD"), cancellable = true)
-    private void onNextCheckIfMapLocationThread(int bits, CallbackInfoReturnable<Integer> cir) {
-        Thread t = this.owner != null ? this.owner.get() : null;
-        if (Bettermaps.locateMapTaskThreads.containsValue(t)) {
-            cir.setReturnValue(super.next(bits));
+            cir.setReturnValue(true);
         }
     }
 }
